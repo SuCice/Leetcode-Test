13202785142f105fec84180eaab579ef01968278
chao.su <chao.su@bianlifeng.com>
2018-04-09 21:55:52 +0800

    add No.1 answer

--- The title and answer/NO.1/two-nums.py
+++ The title and answer/NO.1/two-nums.py
@@ -4,3 +4,11 @@
 # @Email   : mat_wu@163.com
 # @File    : two-nums.py
 # @Software: PyCharm
+class Solution(object):
+    def twoSum(self,nums, target):
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], i]
+            else:
+                dic[target - num] = i
